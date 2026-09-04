a08229c1ce7cc9cb6b894e4e00b505d6d4c9a037
McMnyages <morachasilas@gmail.com>
2026-09-04 22:21:23 +0300

    fix: correct import path for anecdotes service and update store initialization

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,7 +1,7 @@
 import AnecdoteList from "./components/AnecdoteList"
 import AnecdoteForm from "./components/AnecdoteForm"
 import Filter from "./components/Filter"
-import anecdotesService from './services/anacdotes'
+import anecdotesService from './services/anecdotes'
 import Notification from "./components/Notification"
 import { useAnecdotesActions } from './store'
 import { useEffect } from "react"
--- anecdotes/src/store.js
+++ anecdotes/src/store.js
@@ -1,12 +1,15 @@
 import { create } from 'zustand'
 import { useShallow } from 'zustand/react/shallow'
-import anecdoteService from './services/anacdotes'
+import anecdoteService from './services/anecdotes'
 
 const useAnecdoteStore = create((set, get) => ({
   anecdotes: [],
   filters: '',
   actions: {
-    initialize: anecdotes => set({ anecdotes }),
+    initialize: async () => {
+      const anecdotes = await anecdoteService.getAll()
+      set({ anecdotes })
+    },
     vote: async (id) => {
       const anecdote = get().anecdotes.find(a => a.id === id)
       const updatedAnecdote = {
@@ -43,8 +46,12 @@ const useAnecdoteStore = create((set, get) => ({
   },
 }))
 
-export const useAnecdotes = () => useAnecdoteStore(useShallow(({ anecdotes, filters }) => {
-  return anecdotes.filter(anecdote => anecdote.content?.toLowerCase().includes(filters.toLowerCase()))
-}))
+export const useAnecdotes = () =>
+  useAnecdoteStore(useShallow(({ anecdotes, filters }) => {
+    return anecdotes.filter(anecdote =>
+      anecdote.content?.toLowerCase().includes(filters.toLowerCase())).sort((a, b) => b.votes - a.votes)
+  })
+  )
 export const useAnecdotesActions = () => useAnecdoteStore((state) => state.actions)
 export const useFilters = () => useAnecdoteStore((state) => state.filters)
+export default useAnecdoteStore
