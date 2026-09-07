fcaaf99a05a7531cca30c4880c523d6b8caad7bd
McMnyages <morachasilas@gmail.com>
2026-09-07 22:23:01 +0300

    feat: add updateAnecdote function and integrate it into useAnecdotes hook for voting functionality

--- query-anecdotes/src/hooks/useAnecdotes.js
+++ query-anecdotes/src/hooks/useAnecdotes.js
@@ -1,11 +1,12 @@
 import { useQuery,useMutation,useQueryClient } from '@tanstack/react-query'
-import { getAnecdotes,createAnecdote} from '../requests'
+import { getAnecdotes,createAnecdote,updateAnecdote} from '../requests'
 
 const useAnecdotes = () => {
     const queryClient =  useQueryClient()
   const response = useQuery({
     queryKey: ['anecdotes'],
-    queryFn: getAnecdotes
+    queryFn: getAnecdotes,
+    retry:1
   })
 
   const addAnecdote =useMutation({
@@ -15,11 +16,22 @@ const useAnecdotes = () => {
         queryClient.setQueryData(['anecdotes'],anecdotes.concat(newAnecdote))
     }
   })
+
+  const voteMutation = useMutation({
+  mutationFn: updateAnecdote,
+  onSuccess: () => {
+    queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+  },
+})
+
   return {
     anecdotes: response.data,
     isPending: response.isPending,
     isError: response.isError,
-    newAnecdote:(anecdote)=>addAnecdote.mutate(anecdote)
+    newAnecdote:(anecdote)=>addAnecdote.mutate(anecdote),
+    addVote: (anecdote)=>voteMutation.mutate({
+      ...anecdote,votes:anecdote.votes+1
+    })
   }
 }
 
--- query-anecdotes/src/requests.js
+++ query-anecdotes/src/requests.js
@@ -24,3 +24,15 @@ export const createAnecdote = async (content) => {
  
   return await response.json()
 }
+
+export const updateAnecdote = async (anecdote) => {
+  const response = await fetch(`${baseUrl}/${anecdote.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(anecdote),
+  })
+
+  return response.json()
+}
